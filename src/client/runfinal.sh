acb490e2aec9d25c363727a4ba81fdc8422a545a
Robert Reyna <robscodebase@gmail.com>
2018-03-16 04:31:30 -0400

    Vuejs development setup complete with docker compose up

--- src/client/runfinal.sh
+++ src/client/runfinal.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-#docker volume rm tech-router-front-end-src
-docker volume rm tech-router-front-end-dist
-#docker volume create tech-router-front-end-src
-docker volume create tech-router-front-end-dist
 docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)
-docker rmi $(docker images -a | grep "vue-technician-route-planner-install") && docker rmi $(docker images -a | grep "none")
-docker build -t vue-technician-route-planner-install .
-docker run  -itd --name vue-technician-route-planner-install -v /home/robert/technicianrouteplanner/vuesrc:/vue-technician-route-planner vue-technician-route-planner-install
-docker exec -ti vue-technician-route-planner-install /bin/bash
+docker rmi $(docker images -a | grep "technician") && docker rmi $(docker images -a | grep "none")
+docker build -t vue-technician-route-planner .
+docker run  -itd --name vue-technician-route-planner -v /home/robert/technicianrouteplanner/src/vue:/vue-technician-route-planner vue-technician-route-planner
+docker exec -ti vue-technician-route-planner /bin/bash
