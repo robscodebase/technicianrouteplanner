f83078620e98b7172f0a46311fa2538fd1dde15d
Robert Reyna <robscodebase@gmail.com>
2018-03-17 03:20:37 -0400

    dashboard front end edit in progress.

--- composeRestart.sh
+++ composeRestart.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)
-docker rmi $(docker images -a | grep "tech")
-docker rmi $(docker images -a | grep "<none>")
-docker volume rm $(docker volume ls -a)
+#docker rmi $(docker images -a | grep "tech")
+#docker rmi $(docker images -a | grep "<none>")
+#docker volume rm $(docker volume ls -a)
 docker ps -a
 docker images -a
 docker volume ls -a
--- src/vue/src/components/Header/Header.vue
+++ src/vue/src/components/Header/Header.vue
@@ -3,27 +3,13 @@
     <button class="navbar-toggler mobile-sidebar-toggler d-lg-none" type="button" @click="mobileSidebarToggle">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <b-link class="navbar-brand" to="#"></b-link>
     <button class="navbar-toggler sidebar-toggler d-md-down-none" type="button" @click="sidebarToggle">
       <span class="navbar-toggler-icon"></span>
     </button>
     <b-navbar-nav class="d-md-down-none">
-      <b-nav-item class="px-3">Dashboard</b-nav-item>
-      <b-nav-item class="px-3">Users</b-nav-item>
-      <b-nav-item class="px-3">Settings</b-nav-item>
-    </b-navbar-nav>
-    <b-navbar-nav class="ml-auto">
-      <b-nav-item class="d-md-down-none">
-        <i class="icon-bell"></i>
-        <b-badge pill variant="danger">5</b-badge>
-      </b-nav-item>
-      <b-nav-item class="d-md-down-none">
-        <i class="icon-list"></i>
-      </b-nav-item>
-      <b-nav-item class="d-md-down-none">
-        <i class="icon-location-pin"></i>
-      </b-nav-item>
-      <HeaderDropdown/>
+      <b-nav-item class="px-3">Technician Route Planner</b-nav-item>
+      <b-nav-item class="px-3">Add Route</b-nav-item>
+      <b-nav-item class="px-3">Add Technician</b-nav-item>
     </b-navbar-nav>
     <button class="navbar-toggler aside-menu-toggler d-md-down-none" type="button" @click="asideToggle">
       <span class="navbar-toggler-icon"></span>
@@ -31,13 +17,8 @@
   </header>
 </template>
 <script>
-  import HeaderDropdown from './HeaderDropdown.vue'
-
   export default {
     name: 'c-header',
-    components: {
-      HeaderDropdown
-    },
     methods: {
       sidebarToggle (e) {
         e.preventDefault()
