693836bb6c7f247d18698014c2dd8e29f0b87e90
Robert Reyna <robscodebase@gmail.com>
2018-03-21 03:28:55 -0400

    Successful Docker compose with all files and dependencies. TODO: Build out grpc functions in client and server.

--- src/grpc-client/js/build.sh
+++ src/grpc-client/js/build.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+npm install webpack -g
+npm install webpack-cli -g
+npm install
 webpack src/grpc-client.js
--- src/grpc-client/js/src/grpc-client.js
+++ src/grpc-client/js/src/grpc-client.js
@@ -1,1 +1,7 @@
+import {grpc, Code, Metadata} from "grpc-web-client";
+import {routePlanner} from "../proto/technicianRoutePlanner_pb_service";
+import {technicianRoutePlanner} from "../proto/technicianRoutePlanner_pb";
+
+const host = "http://localhost:9090";
+
 alert("hello")
