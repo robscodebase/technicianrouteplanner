06d09ea576ade5ca4c09467e83892e7c5d820561
Robert Reyna <robscodebase@gmail.com>
2018-03-26 03:14:39 -0400

    Updated LICENSE.

--- src/file-server/src/main.go
+++ src/file-server/src/main.go
@@ -1,5 +1,5 @@
 // Copyright (c) 2018 Robert Reyna. All rights reserved.
-// License BSD 3-Clause https://github.com/robscodebase/eventplanner/blob/master/LICENSE.md
+// License BSD 3-Clause https://github.com/robscodebase/technicianrouteplanner/blob/master/LICENSE.md
 
 package main
 
